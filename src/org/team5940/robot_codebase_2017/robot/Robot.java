e8f462c421867d517ddc31a0d698f47a4d2ca37f
David Boles <david.e.boles22@gmail.com>
2017-01-21 15:06:33 -0800

    Made Robot Project

---
+++ src/org/team5940/robot_codebase_2017/robot/Robot.java
@@ -0,0 +1,19 @@
+package org.team5940.robot_codebase_2017.robot;
+
+import org.team5940.robot_core.modules.robot.RobotModule;
+
+public class Robot extends RobotModule {
+
+	@Override
+	public void initialize() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void shutDown() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
