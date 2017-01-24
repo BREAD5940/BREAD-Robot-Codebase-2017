4885401d762637a69a72422b745e45df8577da95
David Boles <david.e.boles22@gmail.com>
2017-01-23 16:54:42 -0800

    Added loggermodule changes

--- src/org/team5940/robot_codebase_2017/robot/Robot.java
+++ src/org/team5940/robot_codebase_2017/robot/Robot.java
@@ -1,5 +1,6 @@
 package org.team5940.robot_codebase_2017.robot;
 
+import org.team5940.robot_core.modules.logging.LoggerModule;
 import org.team5940.robot_core.modules.robot.RobotModule;
 
 public class Robot extends RobotModule {
@@ -14,6 +15,12 @@ public class Robot extends RobotModule {
 	public void shutDown() {
 		// TODO Auto-generated method stub
 
+	}
+
+	@Override
+	public void setLogger(LoggerModule logger) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
