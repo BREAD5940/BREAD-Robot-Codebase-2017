dc4527a68a7245c3cf6488d0352fd70999d109ee
david476 <david.e.boles22@gmail.com>
2017-02-21 18:25:21 -0800

    Updated for Testable motor sets.

--- src/org/team5940/robot_codebase_2017/robot/Robot.java
+++ src/org/team5940/robot_codebase_2017/robot/Robot.java
@@ -2,19 +2,16 @@ package org.team5940.robot_codebase_2017.robot;
 
 import org.team5940.robot_core.modules.actuators.motorsets.CANTalonMotorSetModule;
 import org.team5940.robot_core.modules.actuators.motorsets.MotorSetModule;
+import org.team5940.robot_core.modules.actuators.motorsets.TestableCANTalonMotorSetModule;
 import org.team5940.robot_core.modules.actuators.pistons.DoubleSolenoidPistonModule;
 import org.team5940.robot_core.modules.actuators.pistons.PistonModule;
 import org.team5940.robot_core.modules.actuators.shifters.DoubleSolenoidShifterModule;
 import org.team5940.robot_core.modules.actuators.shifters.ShifterModule;
-import org.team5940.robot_core.modules.aggregates.drivetrains.SimpleTankDrivetrainModule;
-import org.team5940.robot_core.modules.aggregates.drivetrains.TankDrivetrainModule;
 import org.team5940.robot_core.modules.logging.LoggerModule;
 import org.team5940.robot_core.modules.logging.SystemLoggerModule;
 import org.team5940.robot_core.modules.robot.SimpleRobotModule;
 import org.team5940.robot_core.modules.sensors.binary_input.BinaryInputModule;
 import org.team5940.robot_core.modules.sensors.binary_input.DigitalBinaryInputModule;
-import org.team5940.robot_core.modules.testing.TestableModule;
-import org.team5940.robot_core.modules.testing.communication.SmartDashboardTestCommunicationModule;
 
 import com.ctre.CANTalon;
 
@@ -36,12 +33,12 @@ public class Robot extends SimpleRobotModule {
 		frontLeftTalon.setInverted(true);
 		CANTalon backLeftTalon = new CANTalon(4);
 		backLeftTalon.setInverted(true);//TODO Encoder object needs to invert.
-		MotorSetModule leftMotorSet = new CANTalonMotorSetModule("left_motors", robot_logger, new CANTalon[]{frontLeftTalon, backLeftTalon});
+		MotorSetModule leftMotorSet = new TestableCANTalonMotorSetModule("left_motors", robot_logger, new CANTalon[]{frontLeftTalon, backLeftTalon});
 		
 		//RIGHT MOTORS
 		CANTalon frontRightTalon = new CANTalon(1);
 		CANTalon backRightTalon = new CANTalon(2);
-		MotorSetModule rightMotorSet = new CANTalonMotorSetModule("right_motors", robot_logger, new CANTalon[]{frontRightTalon, backRightTalon});
+		MotorSetModule rightMotorSet = new TestableCANTalonMotorSetModule("right_motors", robot_logger, new CANTalon[]{frontRightTalon, backRightTalon});
 		
 		//DRIVETRAIN
 		//TODO wasn't working
@@ -50,29 +47,29 @@ public class Robot extends SimpleRobotModule {
 		//INTAKE
 		CANTalon intakeTalon = new CANTalon(6);
 		intakeTalon.setInverted(true);
-		MotorSetModule intakeMotorSet = new CANTalonMotorSetModule("intake_motor_set", robot_logger, intakeTalon);
+		MotorSetModule intakeMotorSet = new TestableCANTalonMotorSetModule("intake_motor_set", robot_logger, intakeTalon);
 		
-		//ARM
+		//ARM MOTOR
 		CANTalon armTalon = new CANTalon(5);
 		armTalon.setInverted(true);
-		//TODO should be replaced for safe test
 		MotorSetModule armMotorSet = new CANTalonMotorSetModule("arm_motor_set", robot_logger, armTalon);
-		
-		//CUP
-		PistonModule cupPiston = new DoubleSolenoidPistonModule("cup_piston", robot_logger, new DoubleSolenoid(9, 4, 5));
 		
 		//LIMIT SWITCHES
 		DigitalInput upDigitalInput = new DigitalInput(0);
 		BinaryInputModule upLimitSwitch = new DigitalBinaryInputModule("up_limit_switch", robot_logger, upDigitalInput, true);
-		
 		DigitalInput downDigitalInput = new DigitalInput(1);
 		BinaryInputModule downLimitSwitch = new DigitalBinaryInputModule("down_limit_switch", robot_logger, downDigitalInput, true);
+		
+		//CUP
+		PistonModule cupPiston = new DoubleSolenoidPistonModule("cup_piston", robot_logger, new DoubleSolenoid(9, 4, 5));
+		
+		//TODO Make aggregate module for arm.
 		
 		//CLIMBING
 		CANTalon leftClimberTalon = new CANTalon(7);
 		leftClimberTalon.setInverted(true);
 		CANTalon rightClimberTalon = new CANTalon(8);
-		//TODO replace with better test
+		//TODO Make a test that only goes positive and asks to check ratchet
 		MotorSetModule climberMotorSet = new CANTalonMotorSetModule("climber_motor_set", robot_logger, new CANTalon[]{leftClimberTalon, rightClimberTalon});
 	}
 	
